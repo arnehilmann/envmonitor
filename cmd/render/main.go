42ebbb4fb4727b370bac30af8ce58ab19958b32f
Arne Hilmann <arne.hilmann@gmail.com>
2017-03-09 16:15:08 +0100

    use rrd bindings instead calling rrdtool

--- cmd/render/main.go
+++ cmd/render/main.go
@@ -1,114 +1,74 @@
 package main
 
 import (
-    "log"
-    "os/exec"
-    "os"
-    "strconv"
-    "strings"
-    "time"
-    "math"
+	"log"
+	"time"
 
-    "github.com/wcharczuk/go-chart"
-    "github.com/wcharczuk/go-chart/drawing"
+	"github.com/ziutek/rrd"
 
-    . "github.com/arnehilmann/goutils"
+	. "github.com/arnehilmann/goutils"
 )
 
 type Timeline struct {
-    name string
-    epochs []time.Time
-    values []float64
+	name   string
+	epochs []time.Time
+	values []float64
 }
 
 func NewTimeline(name string) Timeline {
-    return Timeline{name, []time.Time{}, []float64{}}
+	return Timeline{name, []time.Time{}, []float64{}}
+}
+
+func DecodeRrd(filename, fun, start, end string) ([]Timeline, error) {
+	info, err := rrd.Info(filename)
+	if err != nil {
+		return nil, err
+	}
+	log.Println(info)
+	step := info["step"].(uint)
+	log.Println(step)
+
+	startTime, err := time.Parse("2006-01-02", start)
+	if err != nil {
+		return nil, err
+	}
+	endTime, err := time.Parse("2006-01-02", end)
+	if err != nil {
+		return nil, err
+	}
+	result, err := rrd.Fetch(filename, fun, startTime, endTime, time.Duration(step)*time.Second)
+	if err != nil {
+		return nil, err
+	}
+
+	var timelines []Timeline
+	var ts time.Time
+	for i, name := range result.DsNames {
+		timelines = append(timelines, NewTimeline(name))
+		for t := 0; t < result.RowCnt; t++ {
+			ts = result.Start.Add(time.Duration(t) * result.Step)
+			timelines[i].epochs = append(timelines[i].epochs, ts)
+			value := result.ValueAt(i, t)
+			timelines[i].values = append(timelines[i].values, value)
+		}
+	}
+	return timelines, nil
+}
+
+func (timeline Timeline) Dump(fun func(...interface{})) {
+	fun("timeline", timeline.name)
+	for i := range timeline.epochs {
+		fun(timeline.epochs[i], timeline.values[i])
+	}
 }
 
 func main() {
-    out, err := exec.Command("rrdtool", "fetch", "res/temperature.rrd", "AVERAGE",
-                             "-s", "02/22/2017", "-e", "02/24/2017").Output()
-                             //"-s", "now - 1 day",
-                             //"-e",  "now - 12 hours").Output()
-    PanicIf(err)
-
-    var timelines []Timeline
-    for _, line := range(strings.Split(string(out), "\n")) {
-        if line == "" {
-            continue
-        }
-        fields := strings.Fields(line)
-        if len(timelines) == 0 {
-            for _, name := range(fields) {
-                timelines = append(timelines, NewTimeline(name))
-            }
-            continue
-        }
-        epoch, err := strconv.Atoi(strings.TrimSuffix(fields[0], ":"))
-        PanicIf(err)
-        for index, field := range(fields[1:]) {
-            value, err := strconv.ParseFloat(field, 32)
-            PanicIf(err)
-            if math.IsNaN(value) {
-                continue
-            }
-            timelines[index].epochs = append(timelines[index].epochs, time.Unix(int64(epoch), 0))
-            timelines[index].values = append(timelines[index].values, float64(value))
-        }
+	timelines, err := DecodeRrd("res/temperature.rrd",
+		"AVERAGE",
+		"2017-02-22",
+		"2017-02-24")
+	PanicIf(err)
+    for _, timeline := range timelines {
+        timeline.Dump(log.Println)
     }
-    for _, line := range(timelines) {
-        log.Println(line.name)
-        for i := range(line.epochs) {
-            log.Println(line.epochs[i], line.values[i])
-        }
-        break
-    }
-
-    log.Println("assembling graph")
-
-    graph := chart.Chart{
-        Width: 400,
-        Height: 200,
-        XAxis: chart.XAxis{
-            Style: chart.Style{
-                Show: true,
-            },
-            ValueFormatter: func (v interface{}) string {
-                return chart.TimeValueFormatterWithFormat(v, "2006-01-02T15:04")},
-        },
-        YAxis: chart.YAxis{
-            Style: chart.Style{
-                Show: true,
-            },
-            ValueFormatter: func (v interface{}) string {
-                return chart.FloatValueFormatterWithFormat(v, "%0.1f")},
-            Ticks: []chart.Tick{
-                chart.Tick{4.0, "4"},
-                chart.Tick{6.0, "6"},
-                chart.Tick{7.0, "7"},
-                chart.Tick{14.0, "14"},
-            },
-        },
-        Series: []chart.Series{
-            chart.TimeSeries{
-                Name: timelines[0].name,
-                XValues: timelines[0].epochs,
-                YValues: timelines[0].values,
-                Style: chart.Style{
-                    Show: true,
-                    StrokeWidth: 5.0,
-                    StrokeColor: drawing.Color{255, 0, 0, 255},
-                },
-            },
-        },
-    }
-    err = graph.Series[0].Validate()
-    PanicIf(err)
-
-    f, err := os.Create("aha.png")
-    PanicIf(err)
-    defer f.Close()
-    graph.Render(chart.PNG, f)
-
-    log.Println("done")
 }
