0eee973423d2b6b91c1cb25363db6298e7866672
Arne Hilmann <arne@hilmann.de>
2017-06-12 10:17:03 +0200

    allow textual spec for multiple graphs

--- cmd/render/main.go
+++ cmd/render/main.go
@@ -5,6 +5,8 @@ import (
 	"math"
 	"os"
 	"path/filepath"
+    "strconv"
+    "strings"
 	"time"
 
 	"github.com/ziutek/rrd"
@@ -53,7 +55,7 @@ func DecodeRrd(filename, fun, start, end string) ([]Timeline, error) {
 	var ts time.Time
 	for i, name := range result.DsNames {
 		timelines = append(timelines, NewTimeline(name))
-		for t := 0; t < result.RowCnt; t++ {
+		for t := 0; t < result.RowCnt - 1; t++ {
 			ts = result.Start.Add(time.Duration(t) * result.Step)
 			value := result.ValueAt(i, t)
 			if !math.IsNaN(value) {
@@ -73,7 +75,80 @@ func (timeline Timeline) Dump(fun func(...interface{})) {
 }
 
 func main() {
-	log.Println("ahhh")
+    width := 800
+    height := 600
+    yaxis_linespacing := 5.0
+    axis_color := "0F0"
+    result := "aha.png"
+
+	c := chart.Chart{
+		Width:  width,
+		Height: height,
+		XAxis: chart.XAxis{
+			Style: chart.Style{
+				Show: true,
+				StrokeColor: drawing.ColorFromHex(axis_color),
+			},
+			ValueFormatter: chart.TimeValueFormatterWithFormat("2006-01-02T15:04"),
+		},
+		YAxis: chart.YAxis{
+			Style: chart.Style{
+				Show:        true,
+				StrokeColor: drawing.ColorFromHex(axis_color),
+			},
+			ValueFormatter: func(v interface{}) string {
+				return chart.FloatValueFormatterWithFormat(v, "%0.1f")
+			},
+			Range: chartcontrib.ContinuousRangeWithTicksLinespacing(yaxis_linespacing),
+		},
+        Series: []chart.Series{},
+    }
+
+    specs := "res/temperature.rrd:0:MAX:2017-02-12:2017-02-23:00F:3" +
+        //""
+        " res/temperature.rrd:0:AVERAGE:2017-02-22:2017-02-23:F00:3"
+        //" res/temperature.rrd:0:MIN:2017-02-12:2017-02-24:00F:3" +
+        //" res/temperature.rrd:0:AVERAGE:2017-02-22:2017-02-24:F00:3"
+
+    for _, spec := range(strings.Split(specs, " ")) {
+        token := strings.Split(spec, ":")
+        filename := token[0]
+        series_index, err := strconv.Atoi(token[1])
+        PanicIf(err)
+        function := token[2]
+        start := token[3]
+        end := token[4]
+        color := token[5]
+        stroke, err := strconv.ParseFloat(token[6], 64)
+        PanicIf(err)
+
+        timelines, err := DecodeRrd(filename, function, start, end)
+        PanicIf(err)
+
+        timelines[series_index].Dump(log.Println)
+
+        ts := chart.TimeSeries{
+                    Name:    timelines[series_index].name,
+                    XValues: timelines[series_index].epochs,
+                    YValues: timelines[series_index].values,
+                    Style: chart.Style{
+                        Show:        true,
+                        StrokeWidth: float64(stroke),
+                        StrokeColor: drawing.ColorFromHex(color),
+                    },
+                }
+
+        c.Series = append(c.Series, ts)
+    }
+
+	f, err := os.Create(result)
+	PanicIf(err)
+	defer f.Close()
+	c.Render(chart.PNG, f)
+	log.Println("chart can be found in", f.Name())
+}
+
+func main_old() {
 	timelines, err := DecodeRrd("res/temperature.rrd",
 		"AVERAGE",
 		"2017-02-22",
@@ -81,11 +156,6 @@ func main() {
 	PanicIf(err)
 	log.Println("--------------------")
 	timelines[0].Dump(log.Println)
-	/*
-	   for _, timeline := range timelines {
-	       timeline.Dump(log.Println)
-	   }
-	*/
 
 	linespacing := 5.0
 
